64f1c7fca2a315a3a5127969bcdc6340e5cf53f0
Michael Woodward <m.woodward1017@gmail.com>
2022-01-15 20:25:45 -0800

    T#159, updated working plug in version of two D model glb and or gltf format

--- threej.js
+++ threej.js
@@ -1,17 +1,29 @@
 import * as THREE from './three.js-master/build/three.module.js'
 import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
 
+// import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+// import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
+// import { GLTFLoader } from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js';
+
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
 //This is where we load the glb file
 const loader = new GLTFLoader()
-loader.load('assets/livingroom.glb', function(glb) {
+loader.load('assets/HollowCathode.gltf', function(glb) {
     console.log(glb)
-    const root = glb.scene;
-    root.scale.set(0.1,0.2,0.1)
+    var model = glb.scene;
+    model.scale.set(0.1,0.2,0.1)
 
-    scene.add(root);
+    const mesh = model.children[ 3 ];
+
+    scene.add(model);
+    gltf.animations;
+    gltf.scene;
+    gltf.scenes;
+    gltf.cameras;
+    gltf.asset;
+
 }, function(xhr){
     console.log((xhr.loaded/xhr.total * 100) + "% loaded")
 }, function(error) {    //If does not get loaded correctly
@@ -37,18 +49,24 @@ const sizes = {
 }
 
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.05, 10)
+//const controls = new OrbitControls(camera, renderer.domElement );
 camera.position.set(0,1,2)
 scene.add(camera)
 
+
 const renderer = new THREE.WebGL1Renderer({
     canvas: canvas
+    
 })
 
 renderer.setSize(sizes.width, sizes.height)
+document.body.appendChild( renderer.domElement);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true
 renderer.gammaOutput = true
+
+
 
 function animate() {
     requestAnimationFrame(animate)
