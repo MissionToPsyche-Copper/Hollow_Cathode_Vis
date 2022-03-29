55283fc49af6971f57613e01369b2ac16f09865f
sandalhathat <sandalhathat@gmail.com>
2022-03-29 09:43:53 -0500

    asdfdsf

--- HollowCathodeOnly/hcv3/Assets/Scripts/ToggleButton.cs
+++ HollowCathodeOnly/hcv3/Assets/Scripts/ToggleButton.cs
@@ -7,17 +7,35 @@ public class ToggleButton : MonoBehaviour
     public Material[] material;
     Renderer rend;
     GameObject hollowCathode;
+    public Material mat1;
+    public Material mat2;
 
 
-    void start() {
+    // void start() {
+    //     rend = GetComponent<Renderer>();
+    //     rend.enabled = true;
+    //     rend.sharedMaterial = material[0];
+    //     hollowCathode = GameObject.Find("Hollow Cathode");
+    //     mat1=Resources.Load("transparentMat") as Material;
+    //     hollowCathode.GetComponent<MeshRenderer>().material = mat1;
+    // }
+
+    public void ChangeMat() {
+
         rend = GetComponent<Renderer>();
         rend.enabled = true;
         rend.sharedMaterial = material[0];
-        
-    }
+        hollowCathode = GameObject.Find("Hollow Cathode");
 
-    public void ChangeMat(string mat) {
-        
+
+
+        if (hollowCathode.GetComponent<MeshRenderer>().material == mat1) {
+            mat2=Resources.Load("solidMat") as Material;
+            hollowCathode.GetComponent<MeshRenderer>().material = mat2;
+        } else if (hollowCathode.GetComponent<MeshRenderer>().material == mat2) {
+            mat1=Resources.Load("transparentMat") as Material;
+        hollowCathode.GetComponent<MeshRenderer>().material = mat1;
+        }
     }
 
 
